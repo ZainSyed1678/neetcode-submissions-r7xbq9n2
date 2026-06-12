b47e2a1beb1f64e08a1c19f74a1a0fca86bc7f5d
ZainSyed1678 <zainwasimsyed@gmail.com>
2026-06-12 16:02:15 +0530

    Add: maximum-number-of-balloons - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        freq = {}
+        for c in text:
+            freq[c] = freq.get(c,0) + 1
+
+        return min(
+            freq.get("b",0),
+            freq.get("a",0),
+            freq.get("l",0) // 2,
+            freq.get("o",0) // 2,
+            freq.get("n",0)
+        )
